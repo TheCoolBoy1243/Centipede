e560ea97f942035cf1e4eb01cae04bff33563164
Bas Pijls <bpijls@gmail.com>
2018-03-12 21:23:19 +0100

    Adds GameManagement framework. Makes Centipede inherit from GameEnvironment

--- Centipede/Program.cs
+++ Centipede/Program.cs
@@ -14,7 +14,7 @@ namespace Centipede
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new Centipede())
                 game.Run();
         }
     }
